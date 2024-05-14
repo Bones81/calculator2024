d5f973e8ece1013f1eb83af8c9371054d114636f
Nathan Freeman <nathanlfreeman@gmail.com>
2024-05-14 18:51:15 -0400

    Fix operate function

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const display = document.querySelector('#display')
+// SET SELECTORS AND BUTTON REFERENCES
+// const display = document.querySelector('#display')
+// const btn1 = document.querySelector('1-btn')
+// const btn2 = document.querySelector('2-btn')
+// const btn3 = document.querySelector('3-btn')
+// const btn4 = document.querySelector('4-btn')
+// const btn5 = document.querySelector('5-btn')
+// const btn6 = document.querySelector('6-btn')
+// const btn7 = document.querySelector('7-btn')
+// const btn8 = document.querySelector('8-btn')
+// const btn9 = document.querySelector('9-btn')
+// const btn0 = document.querySelector('0-btn')
+// const plusMinusBtn = document.querySelector('plus-minus-btn')
+// const decimalBtn = document.querySelector('decimal-btn')
+// const allClearBtn = document.querySelector('all-clear-btn')
+// const clearBtn = document.querySelector('clear-btn')
+// const addBtn = document.querySelector('add-btn')
+// const subtractBtn = document.querySelector('subtract-btn')
+// const multiplyBtn = document.querySelector('multiply-btn')
+// const divideBtn = document.querySelector('divide-btn')
+// const equalsBtn = document.querySelector('equals-btn')
 
+// SET OPERATOR FUNCTIONS
 function add(a,b) {
     return a + b
 }
@@ -17,12 +38,31 @@ function divide(a,b) {
     return b / a
 }
 
+// SET OPERATE FUNCTION
 function operate(op, a, b) {
-    return op(a , b)
+    switch(op) {
+        case '+':
+            return add(a,b)
+            break
+        case '-':
+            return subtract(a,b)
+            break
+        case '*':
+            return multiply(a,b)
+            break;
+        case '/':
+            return divide(a,b)
+            break;
+        default:
+            alert('An error has occurred')
+    }
 }
 
-function addDecimal() {
-    if(display.textContent.includes('.')) return
-    display.innerText += '.'
-}
+// SET DECIMAL LOGIC
+// function addDecimal() {
+//     if(display.textContent.includes('.')) return
+//     display.innerText += '.'
+// }
+
+console.log(operate('+',2,3))
 
