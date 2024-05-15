1bcd9e3aa5b442689edaaf223c3d5ad05a3e85d9
Nathan Freeman <nathanlfreeman@gmail.com>
2024-05-15 19:10:06 -0400

    Add display font; Add basic styles
    Add Digital7 font. Apply flex style to buttons. Add basic color scheme.
    Set relative widths of buttons and display. Set font-sizes. Set body of
    calculator size (for now). Add shadows to body and branding header.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // SET SELECTORS AND BUTTON REFERENCES
-// const display = document.querySelector('#display')
-// const btn1 = document.querySelector('1-btn')
-// const btn2 = document.querySelector('2-btn')
-// const btn3 = document.querySelector('3-btn')
-// const btn4 = document.querySelector('4-btn')
-// const btn5 = document.querySelector('5-btn')
-// const btn6 = document.querySelector('6-btn')
-// const btn7 = document.querySelector('7-btn')
-// const btn8 = document.querySelector('8-btn')
-// const btn9 = document.querySelector('9-btn')
-// const btn0 = document.querySelector('0-btn')
-// const plusMinusBtn = document.querySelector('plus-minus-btn')
-// const decimalBtn = document.querySelector('decimal-btn')
-// const allClearBtn = document.querySelector('all-clear-btn')
-// const clearBtn = document.querySelector('clear-btn')
-// const addBtn = document.querySelector('add-btn')
-// const subtractBtn = document.querySelector('subtract-btn')
-// const multiplyBtn = document.querySelector('multiply-btn')
-// const divideBtn = document.querySelector('divide-btn')
-// const equalsBtn = document.querySelector('equals-btn')
+const display = document.querySelector('#display')
+const btn1 = document.querySelector('1-btn')
+const btn2 = document.querySelector('2-btn')
+const btn3 = document.querySelector('3-btn')
+const btn4 = document.querySelector('4-btn')
+const btn5 = document.querySelector('5-btn')
+const btn6 = document.querySelector('6-btn')
+const btn7 = document.querySelector('7-btn')
+const btn8 = document.querySelector('8-btn')
+const btn9 = document.querySelector('9-btn')
+const btn0 = document.querySelector('0-btn')
+const plusMinusBtn = document.querySelector('plus-minus-btn')
+const decimalBtn = document.querySelector('decimal-btn')
+const allClearBtn = document.querySelector('all-clear-btn')
+const clearBtn = document.querySelector('clear-btn')
+const addBtn = document.querySelector('add-btn')
+const subtractBtn = document.querySelector('subtract-btn')
+const multiplyBtn = document.querySelector('multiply-btn')
+const divideBtn = document.querySelector('divide-btn')
+const equalsBtn = document.querySelector('equals-btn')
 
 // SET OPERATOR FUNCTIONS
 function add(a,b) {
@@ -59,10 +59,10 @@ function operate(op, a, b) {
 }
 
 // SET DECIMAL LOGIC
-// function addDecimal() {
-//     if(display.textContent.includes('.')) return
-//     display.innerText += '.'
-// }
+function addDecimal() {
+    if(display.textContent.includes('.')) return
+    display.innerText += '.'
+}
 
-console.log(operate('+',2,3))
+const buttons = document.querySelectorAll('.buttons')
 
